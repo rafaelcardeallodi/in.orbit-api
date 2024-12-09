2d8d941251fa4d9cd6796e34912a2a54b3035b9a
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-12-09 20:59:34 -0300

    Jwt e Rota de Autenticação

--- src/http/server.ts
+++ src/http/server.ts
@@ -13,6 +13,7 @@ import { getWeekSummaryRoute } from './routes/get-week-summary'
 import { fastifyCors } from '@fastify/cors'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { authenticateFromGithubRoute } from './routes/authenticate-from-github'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -37,6 +38,7 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/docs',
 })
 
+app.register(authenticateFromGithubRoute)
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
