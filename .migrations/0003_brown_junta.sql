4d0e090fa1c8ca5cdc63287cb838ff67fa23ec9f
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-12-09 20:22:08 -0300

    Criando Tabela de Usuários

---
+++ .migrations/0003_brown_junta.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "goals" ADD COLUMN "user_id" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "goals" ADD CONSTRAINT "goals_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,17 +1,27 @@
 import dayjs from 'dayjs'
 import { client, db } from '.'
-import { goalCompletions, goals } from './schema'
+import { goalCompletions, goals, users } from './schema'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
+  const [user] = await db
+    .insert(users)
+    .values({
+      name: 'Rafael Lodi',
+      email: null,
+      externalAccountId: 65565453,
+      avatarUrl: 'https://avatars.githubusercontent.com/u/65565453',
+    })
+    .returning()
+
   const result = await db
     .insert(goals)
     .values([
-      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-      { title: 'Me exercitar', desiredWeeklyFrequency: 3 },
-      { title: 'Meditar', desiredWeeklyFrequency: 1 },
+      { title: 'Acordar cedo', userId: user.id, desiredWeeklyFrequency: 5 },
+      { title: 'Me exercitar', userId: user.id, desiredWeeklyFrequency: 3 },
+      { title: 'Meditar', userId: user.id, desiredWeeklyFrequency: 1 },
     ])
     .returning()
 
