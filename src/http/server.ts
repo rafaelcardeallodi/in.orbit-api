24ad7e4b51b8ffc099153ffb043eb6563886e1f5
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-12-09 21:21:31 -0300

    Rota de Perfil

--- src/http/server.ts
+++ src/http/server.ts
@@ -16,6 +16,7 @@ import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { authenticateFromGithubRoute } from './routes/authenticate-from-github'
 import { fastifyJwt } from '@fastify/jwt'
 import { env } from '../env'
+import { getProfileRoute } from './routes/get-profile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -45,6 +46,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(authenticateFromGithubRoute)
+app.register(getProfileRoute)
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
