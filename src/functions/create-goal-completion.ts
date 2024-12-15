242b2e169c2e6902a651afac68210c67b28899ea
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-12-15 16:31:27 -0300

    Setup da Gamificação

--- src/functions/create-goal-completion.ts
+++ src/functions/create-goal-completion.ts
@@ -1,6 +1,6 @@
 import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
 import { db } from '../db'
-import { goalCompletions, goals } from '../db/schema'
+import { goalCompletions, goals, users } from '../db/schema'
 import dayjs from 'dayjs'
 
 interface CreateGoalCompletionRequest {
@@ -53,11 +53,25 @@ export async function createGoalCompletion({
     throw new Error('Goal already completed this week!')
   }
 
-  const insertResult = await db
-    .insert(goalCompletions)
-    .values({ goalId })
-    .returning()
-  const goalCompletion = insertResult[0]
+  const isLastCompletionFromGoal =
+    completionCount + 1 === desiredWeeklyFrequency
+  const earnedExperience = isLastCompletionFromGoal ? 7 : 5
+
+  const goalCompletion = await db.transaction(async () => {
+    const [goalCompletion] = await db
+      .insert(goalCompletions)
+      .values({ goalId })
+      .returning()
+
+    await db
+      .update(users)
+      .set({
+        experience: sql /*sql*/`${users.experience} + ${earnedExperience}`,
+      })
+      .where(eq(users.id, userId))
+
+    return goalCompletion
+  })
 
   return {
     goalCompletion,
