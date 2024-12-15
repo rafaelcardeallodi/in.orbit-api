25e78fd7330cd6ed209cd38d8c433c84b37278c8
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2024-12-15 18:17:57 -0300

    ci: add .env.test

--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,11 @@
 import z from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(['development', 'production', 'test'])
-    .optional()
-    .default('production'),
+  NODE_ENV: z.enum(['development', 'test', 'production']),
   DATABASE_URL: z.string().url(),
 
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
+  GITHUB_OAUTH_CLIENT_ID: z.string(),
+  GITHUB_OAUTH_CLIENT_SECRET: z.string(),
 
   JWT_SECRET: z.string(),
 })
--- src/modules/github-oauth.ts
+++ src/modules/github-oauth.ts
@@ -13,8 +13,11 @@ interface GetUserResponse {
 
 export async function getAccessTokenFromCode(code: string) {
   const accessTokenUrl = new URL('https://github.com/login/oauth/access_token')
-  accessTokenUrl.searchParams.set('client_id', env.GITHUB_CLIENT_ID)
-  accessTokenUrl.searchParams.set('client_secret', env.GITHUB_CLIENT_SECRET)
+  accessTokenUrl.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
+  accessTokenUrl.searchParams.set(
+    'client_secret',
+    env.GITHUB_OAUTH_CLIENT_SECRET
+  )
   accessTokenUrl.searchParams.set('code', code)
 
   const response = await fetch(accessTokenUrl, {
